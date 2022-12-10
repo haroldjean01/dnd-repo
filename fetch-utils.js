7359143bbbe612957a48b16da4a67aa43ecd1426
MontanoDiego <diego.e2.montano@gmail.com>
2022-12-09 17:05:53 -0800

    added API keys to fetch-utils.js

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://fgfwcgpgwqwvqbgpdmlr.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnZndjZ3Bnd3F3dnFiZ3BkbWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA2MzM5OTIsImV4cCI6MTk4NjIwOTk5Mn0.0cOL1g2WEjhIpHM1mIvwvjlmXxgDYF7yrMHX3FQzziU';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
