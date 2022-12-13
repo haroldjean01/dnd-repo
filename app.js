b91a9fba61f3cb95e2c58c542461ca8bb83686f9
Aaron Ibarra <aaronibarra132@gmail.com>
2022-12-13 10:06:55 -0800

    Player and DM redirect completed in app.js and player page (folder) created with html, css, and js files

--- app.js
+++ app.js
@@ -3,9 +3,17 @@
 import './auth/user.js';
 
 /* Get DOM Elements */
+const playerBtn = document.getElementById('player-button');
+const dmBtn = document.getElementById('dm-button');
 
 /* State */
 
 /* Events */
+playerBtn.addEventListener('click', () => {
+    window.location.replace('/player');
+});
 
+dmBtn.addEventListener('click', () => {
+    window.location.replace('/battle');
+});
 /* Display Functions */
